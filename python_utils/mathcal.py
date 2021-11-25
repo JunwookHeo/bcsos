5187e87e8a0a63a6ad55d0d496307d661c419261
junwookheo <junwookheo@gmail.com>
2021-11-25 12:20:11 +1000

    change Visualizaing

--- python_utils/mathcal.py
+++ python_utils/mathcal.py
@@ -10,6 +10,20 @@ def pa(p, n):
     P_A = math.exp(-p*n) * sump(p*n, n)  
     return P_A
 
-ns = [1, 2, 4, 8, 16]
-for i in ns:
-    print(i, pa(0.5, i)*100)
+# ns = [1, 2, 4, 8, 16]
+# for i in ns:
+#     print(i, pa(0.5, i)*100)
+
+ns = [8, 4, 2, 1]
+luni = [60.623815,  65.658131,  85.029631,  100.0]
+luni = [1. - n / 100. for n in luni]
+
+for n, l in zip(ns, luni):
+    print(n, l, pa(l, n) * 100)
+
+lexpo = [81.26491,  89.440182,  96.037027,  100.0]
+lexpo = [1. - n / 100. for n in lexpo]
+
+for n, l in zip(ns, lexpo):
+    print(n, l, pa(l, n) * 100)
+    
