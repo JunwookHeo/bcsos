76fd42bacbce5abc828db76e5e2ee25bf765b419
junwookheo <junwookheo@gmail.com>
2022-10-19 08:45:08 +1000

    Added bitcoin parser

--- blockchainsim/simulation/simagent_test.go
+++ blockchainsim/simulation/simagent_test.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/junwookheo/bcsos/common/bitcoin"
 	"github.com/junwookheo/bcsos/common/dbagent"
 )
 
@@ -20,7 +21,7 @@ const DB_PATH_TEST = "../bc_dummy.db"
 
 const SIZE_BH = 80
 
-type bitcoinheader struct {
+type btcblheader struct {
 	Version    uint32
 	PreHash    [32]byte
 	MerkelRoot [32]byte
@@ -29,7 +30,7 @@ type bitcoinheader struct {
 	Nonce      uint32
 }
 
-func TestLoadFromJson(t *testing.T) {
+func TestLoadBlockFromJson(t *testing.T) {
 	f, err := os.Open(PATH_TEST)
 	if err != nil {
 		log.Printf("Open error : %v", err)
@@ -58,8 +59,10 @@ func TestLoadFromJson(t *testing.T) {
 			hash := b[key].(map[string]interface{})["decoded_raw_block"].(map[string]interface{})["hash"]
 			// time := b[key].(map[string]interface{})["decoded_raw_block"].(map[string]interface{})["time"]
 			raw := b[key].(map[string]interface{})["raw_block"].(string)
-			bh := bitcoinheader{}
+			bh := btcblheader{}
 			rawb, _ := hex.DecodeString(raw)
+
+			// Parse Block Header
 			err = binary.Read(bytes.NewBuffer(rawb), binary.LittleEndian, &bh)
 			if err != nil {
 				log.Panicln(err)
@@ -83,8 +86,229 @@ func TestLoadFromJson(t *testing.T) {
 			log.Printf("Block : %x-%v-%v", bh.Version, bh.Timestamp, hex.EncodeToString(bh.PreHash[:]))
 			log.Printf("Block : %v-%v-%v", hex.EncodeToString(bh.MerkelRoot[:]), bh.Difficulty, bh.Nonce)
 
+			// Parse Transactions
+			b := byte(0)
+			err = binary.Read(bytes.NewBuffer(rawb[80:]), binary.LittleEndian, &b)
+			if err != nil {
+				log.Panicln(err)
+			}
+			log.Printf("Count : %x", b)
+
 		}
 
+	}
+}
+
+func TestLoadTransactionFromJson(t *testing.T) {
+	f, err := os.Open(PATH_TEST)
+	if err != nil {
+		log.Printf("Open error : %v", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(f)
+	const maxCapacity = 16 * 1025 * 1024
+	buf := make([]byte, maxCapacity)
+	scanner.Buffer(buf, maxCapacity)
+
+	for scanner.Scan() {
+		var rec map[string]interface{}
+		err := json.Unmarshal([]byte(scanner.Text()), &rec)
+		if err != nil {
+			panic(err)
+		}
+
+		// var b map[string]interface{}
+		b, ok := rec["data"].(map[string]interface{})
+		if !ok {
+			log.Panicln("Read block type error")
+		}
+
+		for key := range b {
+			raw := b[key].(map[string]interface{})["raw_block"].(string)
+			rawb, _ := hex.DecodeString(raw)
+
+			// Parse Transactions
+			pos := SIZE_BH
+			b := byte(0)
+			err = binary.Read(bytes.NewBuffer(rawb[pos:]), binary.LittleEndian, &b)
+			if err != nil {
+				log.Panicln(err)
+			}
+			log.Printf("Count : %x", b)
+			pos += 1
+
+			get_tx_count := func(buf interface{}) {
+				err = binary.Read(bytes.NewBuffer(rawb[pos:]), binary.LittleEndian, buf)
+				if err != nil {
+					log.Panicln(err)
+				}
+			}
+			numtx := uint64(0)
+			if b == 0xfd {
+				buf_s := uint16(0)
+				get_tx_count(&buf_s)
+				numtx = uint64(buf_s)
+				pos += 2
+			} else if b == 0xfe {
+				buf_i := uint32(0)
+				get_tx_count(buf_i)
+				numtx = uint64(buf_i)
+				pos += 4
+			} else if b == 0xff {
+				buf_l := uint64(0)
+				get_tx_count(buf_l)
+				numtx = uint64(buf_l)
+				pos += 8
+			} else {
+				buf_b := uint8(0)
+				get_tx_count(buf_b)
+				numtx = uint64(buf_b)
+				pos += 1
+			}
+
+			log.Printf("Transaction count : %v", numtx)
+			version := uint32(0)
+			err = binary.Read(bytes.NewBuffer(rawb[pos:]), binary.LittleEndian, &version)
+			if err != nil {
+				log.Panicln(err)
+			}
+			log.Printf("Version : %x", version)
+			pos += 4
+
+			numipn := uint64(0)
+			if b == 0xfd {
+				buf_s := uint16(0)
+				get_tx_count(&buf_s)
+				numipn = uint64(buf_s)
+				pos += 2
+			} else if b == 0xfe {
+				buf_i := uint32(0)
+				get_tx_count(buf_i)
+				numipn = uint64(buf_i)
+				pos += 4
+			} else if b == 0xff {
+				buf_l := uint64(0)
+				get_tx_count(buf_l)
+				numipn = uint64(buf_l)
+				pos += 8
+			} else {
+				buf_b := uint8(0)
+				get_tx_count(buf_b)
+				numipn = uint64(buf_b)
+				pos += 1
+			}
+			log.Printf("Input count : %d", numipn)
+
+		}
+
+	}
+}
+
+func TestLoadBlockFromJson2(t *testing.T) {
+	f, err := os.Open(PATH_TEST)
+	if err != nil {
+		log.Printf("Open error : %v", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(f)
+	const maxCapacity = 16 * 1025 * 1024
+	buf := make([]byte, maxCapacity)
+	scanner.Buffer(buf, maxCapacity)
+
+	for scanner.Scan() {
+		var rec map[string]interface{}
+		err := json.Unmarshal([]byte(scanner.Text()), &rec)
+		if err != nil {
+			panic(err)
+		}
+
+		// var b map[string]interface{}
+		b, ok := rec["data"].(map[string]interface{})
+		if !ok {
+			log.Panicln("Read block type error")
+		}
+
+		for key := range b {
+			raw := b[key].(map[string]interface{})["raw_block"].(string)
+			rb := bitcoin.NewRawBlock(raw)
+			version := rb.ReadUint32()
+			prehash := rb.ReverseBuf(rb.ReadBytes(32))
+			merkleroot := rb.ReverseBuf(rb.ReadBytes(32))
+			timestamp := rb.ReadUint32()
+			difficulty := rb.ReadUint32()
+			nonce := rb.ReadUint32()
+			txcount := rb.ReadVariant()
+			log.Printf("Version : %x", version)
+			log.Printf("Pev Hash : %v", hex.EncodeToString(prehash))
+			log.Printf("Merkle Root : %v", hex.EncodeToString(merkleroot))
+			log.Printf("Timestamp : %d", timestamp)
+			log.Printf("Dificulty : %d", difficulty)
+			log.Printf("Nonce : %d", nonce)
+			log.Printf("Tx Count : %d", txcount)
+
+			for i := 0; i < int(txcount); i++ {
+				log.Printf("Tx(%v) =================================", i)
+				version := rb.ReadUint32()
+				witflag := rb.ReadOptional(2)
+				if witflag != nil && witflag[0] == 0x00 && witflag[1] == 0x01 {
+					rb.IncreasePos(2)
+				}
+				incount := rb.ReadVariant()
+				log.Printf("Version : %x", version)
+				log.Printf("Witness Flag : %v", witflag)
+				log.Printf("Input count : %v", incount)
+
+				for j := 0; j < int(incount); j++ {
+					preout := rb.ReadBytes(32)
+					locktime := rb.ReadUint32()
+					scrlen := rb.ReadVariant()
+
+					log.Printf("Previous Output : %v", preout)
+					log.Printf("Lock Time : %v", locktime)
+					log.Printf("Script Length : %v", scrlen)
+
+					if scrlen > 0 {
+						script := rb.ReadBytes(uint32(scrlen))
+						log.Printf("Unlock Script : %x", script[0])
+					}
+
+					seqnumber := rb.ReadUint32()
+					log.Printf("Sequence Number : %v", seqnumber)
+				}
+
+				outlen := rb.ReadVariant()
+				log.Printf("Output count : %v", outlen)
+				for k := 0; k < int(outlen); k++ {
+					txvalue := rb.ReadUint64()
+					scrlen := rb.ReadVariant()
+					script := rb.ReadBytes(uint32(scrlen))
+					log.Printf("%v Transaction Value : %v", k, txvalue)
+					log.Printf("%v scrlen : %v", k, scrlen)
+					log.Printf("%v Lock Script : %x", k, script[0])
+
+				}
+
+				if witflag != nil && witflag[0] == 0x00 && witflag[1] == 0x01 {
+					for j := 0; j < int(incount); j++ {
+						witcnt := rb.ReadVariant()
+						log.Printf("Witness count : %v", witcnt)
+						for l := 0; l < int(witcnt); l++ {
+							witlen := rb.ReadVariant()
+							log.Printf("Witness length : %v", witlen)
+							witness := rb.ReadBytes(uint32(witlen))
+							log.Printf("Witness : %v", witness)
+						}
+					}
+				}
+
+				locktime := rb.ReadUint32()
+				log.Printf("Lock Time : %v", locktime)
+
+			}
+			return
+		}
 	}
 }
 
---
+++ common/bitcoin/rawblock.go
@@ -0,0 +1,120 @@
+package bitcoin
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"log"
+)
+
+type RawBlock struct {
+	rawBuf []byte
+	pos    uint32
+}
+
+func NewRawBlock(raw string) *RawBlock {
+	rawb, err := hex.DecodeString(raw)
+	if err != nil {
+		log.Panicf("NewRawBlock Error : %v", err)
+		return nil
+	}
+	return &RawBlock{rawBuf: rawb, pos: 0}
+}
+
+func (rb *RawBlock) ReadBytes(len uint32) []byte {
+	buf := make([]byte, len)
+	err := binary.Read(bytes.NewBuffer(rb.rawBuf[rb.pos:]), binary.LittleEndian, &buf)
+	if err != nil {
+		log.Panicln(err)
+		return nil
+	}
+
+	rb.pos += len
+	return buf
+}
+
+func (rb *RawBlock) ReadUint8() uint8 {
+	buf := make([]byte, 1)
+	err := binary.Read(bytes.NewBuffer(rb.rawBuf[rb.pos:]), binary.LittleEndian, &buf)
+	if err != nil {
+		log.Panicln(err)
+		return 0
+	}
+
+	rb.pos += 1
+	return buf[0]
+}
+
+func (rb *RawBlock) ReadUint16() uint16 {
+	buf := make([]byte, 2)
+	err := binary.Read(bytes.NewBuffer(rb.rawBuf[rb.pos:]), binary.LittleEndian, &buf)
+	if err != nil {
+		log.Panicln(err)
+		return 0
+	}
+
+	rb.pos += 2
+	return binary.LittleEndian.Uint16(buf)
+}
+
+func (rb *RawBlock) ReadUint32() uint32 {
+	buf := make([]byte, 4)
+	err := binary.Read(bytes.NewBuffer(rb.rawBuf[rb.pos:]), binary.LittleEndian, &buf)
+	if err != nil {
+		log.Panicln(err)
+		return 0
+	}
+
+	rb.pos += 4
+	return binary.LittleEndian.Uint32(buf)
+}
+
+func (rb *RawBlock) ReadUint64() uint64 {
+	buf := make([]byte, 8)
+	err := binary.Read(bytes.NewBuffer(rb.rawBuf[rb.pos:]), binary.LittleEndian, &buf)
+	if err != nil {
+		log.Panicln(err)
+		return 0
+	}
+
+	rb.pos += 8
+	return binary.LittleEndian.Uint64(buf)
+}
+
+func (rb *RawBlock) ReadVariant() uint64 {
+	t := rb.ReadUint8()
+	var val uint64
+	if t == 0xfd {
+		val = uint64(rb.ReadUint16())
+	} else if t == 0xfe {
+		val = uint64(rb.ReadUint32())
+	} else if t == 0xff {
+		val = uint64(rb.ReadUint64())
+	} else {
+		val = uint64(t)
+	}
+	return val
+}
+
+func (rb *RawBlock) ReadOptional(len uint32) []byte {
+	buf := make([]byte, len)
+	err := binary.Read(bytes.NewBuffer(rb.rawBuf[rb.pos:]), binary.LittleEndian, &buf)
+	if err != nil {
+		log.Panicln(err)
+		return nil
+	}
+
+	return buf
+}
+
+func (rb *RawBlock) IncreasePos(len uint32) {
+	rb.pos += len
+}
+
+func (rb *RawBlock) ReverseBuf(buf []byte) []byte {
+	n := len(buf)
+	for i := 0; i < n/2; i++ {
+		buf[i], buf[n-1-i] = buf[n-1-i], buf[i]
+	}
+	return buf
+}
