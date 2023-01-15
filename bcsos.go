ede6ae278489c395c3a452ad832052a51af440b3
junwookheo <junwookheo@gmail.com>
2023-01-15 14:01:23 +1000

    Add encryption/decryption of prime field

--- bcsos.go
+++ bcsos.go
@@ -20,216 +20,6 @@ func init() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
 }
 
-// func test_gf_mult() {
-// 	gfpoly, err := galois.GF(16)
-// 	if err != nil {
-// 		log.Println("GF(1) should rise error")
-// 	}
-
-// 	start := time.Now().UnixNano()
-// 	var cal1 uint32
-// 	var cal2 uint64
-// 	max_len := 65536
-// 	for i := 0; i < max_len; i++ {
-// 		for j := 0; j < max_len; j++ {
-// 			cal1, _ = gfpoly.Mul(uint32(i), uint32(j))
-// 			cal2 = gfpoly.MulN(uint64(i), uint64(j))
-// 			if cal1 != uint32(cal2) {
-// 				log.Printf("Fail (%v, %v) : %v - %v", i, j, cal1, cal2)
-// 				return
-// 			}
-// 		}
-// 	}
-// 	end := time.Now().UnixNano()
-// 	log.Printf("Time for EXP1 : %v", (end-start)/1000)
-// }
-
-// func test_gf_div() {
-// 	gfpoly, err := galois.GF(16)
-// 	if err != nil {
-// 		log.Println("GF(1) should rise error")
-// 	}
-
-// 	start := time.Now().UnixNano()
-// 	var cal1 uint32
-// 	var cal2 uint64
-// 	max_len := 65536
-// 	for i := 65536 / 2; i < max_len; i++ {
-// 		for j := 0; j < max_len; j++ {
-// 			cal1, _ = gfpoly.Div(uint32(i), uint32(j))
-// 			cal2 = gfpoly.DivN(uint64(i), uint64(j))
-// 			if cal1 != uint32(cal2) {
-// 				log.Printf("Fail (%v, %v) : %v - %v", i, j, cal1, cal2)
-// 				return
-// 			}
-// 		}
-// 	}
-// 	end := time.Now().UnixNano()
-// 	log.Printf("Time for EXP1 : %v", (end-start)/1000)
-// }
-
-// func test_gf_exp() {
-// 	gfpoly, err := galois.GF(16)
-// 	if err != nil {
-// 		log.Println("GF(1) should rise error")
-// 	}
-
-// 	start := time.Now().UnixNano()
-// 	var cal1 uint32
-// 	var cal2 uint64
-// 	max_len := 65536
-// 	for i := 0; i < max_len; i++ {
-// 		for j := 0; j < max_len; j++ {
-// 			cal1, _ = gfpoly.Expon(uint32(i), uint32(j))
-// 			cal2 = gfpoly.Exp(uint64(i), uint64(j))
-// 			if cal1 != uint32(cal2) {
-// 				log.Printf("Fail (%v, %v) : %v - %v", i, j, cal1, cal2)
-// 				return
-// 			}
-// 		}
-// 	}
-// 	end := time.Now().UnixNano()
-// 	log.Printf("Time for EXP1 : %v", (end-start)/1000)
-// }
-
-// 2^32 :
-//   - 2P -1 :  7 * 1227133513
-//   - 3P -2 :  2 * 6442450943
-func test_gf_exp2() {
-	gfpoly := galois.GFN(32)
-	if gfpoly == nil {
-		log.Println("GF(1) should rise error")
-		return
-	}
-
-	start := time.Now().UnixNano()
-	var cal1 uint64
-	var cal2 uint64
-	max_len := 4294967296
-	for i := 0; i < max_len; i++ {
-		cal1 = gfpoly.Exp(uint64(i), 2)
-		cal2 = gfpoly.Exp(uint64(cal1), 6442450943)
-		if uint64(i) != cal2 {
-			log.Printf("ERROR   %v : %v <==> %v", i, cal1, cal2)
-			break
-		}
-	}
-	end := time.Now().UnixNano()
-	log.Printf("Time for EXP1 : %v", (end-start)/1000)
-}
-
-// 2^32 :
-//   - 2P -1 :  7 * 1227133513
-//   - 3P -2 :  2 * 6442450943
-func test_gf_exp3() {
-	gfpoly := galois.GFN(32)
-	if gfpoly == nil {
-		log.Println("GF(1) should rise error")
-		return
-	}
-
-	start := time.Now().UnixNano()
-	var cal uint64
-	max_len := 4294967296
-	for i := 1; i < max_len; i += 4096 {
-		cal = gfpoly.Exp(uint64(i), 2)
-	}
-	end := time.Now().UnixNano()
-	log.Printf("Time for EXP1 : %v", (end-start)/1000000)
-	log.Printf("<==> %v", cal)
-
-	start = time.Now().UnixNano()
-	for i := 1; i < max_len; i += 4096 {
-		cal = gfpoly.Exp(uint64(i), 6442450943)
-	}
-	end = time.Now().UnixNano()
-	log.Printf("Time for EXP2 : %v", (end-start)/1000000)
-	log.Printf("<==> %v", cal)
-}
-
-func test_gf_div2() {
-	gfpoly := galois.GFN(32)
-	if gfpoly == nil {
-		log.Println("GF(1) should rise error")
-		return
-	}
-
-	start := time.Now().UnixNano()
-	var cal1 uint64
-	var cal2 uint64
-	max_len := 4294967296
-	for i := 1; i < max_len; i += 3333 {
-		for j := 1; j < max_len; j += 3333 {
-			cal1 = gfpoly.Mul(uint64(i), uint64(j))
-			cal2 = gfpoly.Div(uint64(cal1), uint64(j))
-			if uint64(i) != cal2 {
-				log.Printf("ERROR   %v : %v <==> %v", i, cal1, cal2)
-				log.Printf("ERROR %v <==> %v", i, j)
-				return
-			}
-		}
-		log.Printf("PROCESS %v", i)
-	}
-	end := time.Now().UnixNano()
-	log.Printf("Time for EXP1 : %v", (end-start)/1000)
-}
-
-func test_encypt_1() {
-	x := []uint32{21, 32, 43, 54, 65, 76, 87, 98, 19, 20, 31}
-	k := []uint32{19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 19}
-	y := make([]uint32, len(x))
-
-	gfpoly := galois.GFN(32)
-	if gfpoly == nil {
-		log.Println("GF(1) should rise error")
-		return
-	}
-
-	pre := uint32(0)
-	for i := 0; i < len(x); i++ {
-		d := (x[i] ^ k[i]) ^ pre
-		y[i] = uint32(gfpoly.Exp(uint64(d), 6442450943))
-		pre = y[i]
-		log.Printf("X:%v, d:%v, y:%v", x[i], d, y[i])
-	}
-
-	pre = 0
-	for i := 0; i < len(y); i++ {
-		d := uint32(gfpoly.Exp(uint64(y[i]), 2))
-		x[i] = (d ^ pre) ^ k[i]
-		pre = y[i]
-		log.Printf("X:%v, d:%v, y:%v", x[i], d, y[i])
-	}
-}
-
-func test_encypt_2() {
-	x := []uint32{21, 32, 43, 54, 65, 76, 87, 98, 19, 20, 31}
-	k := []uint32{19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 19}
-	y := make([]uint32, len(x))
-
-	gfpoly := galois.GFN(32)
-	if gfpoly == nil {
-		log.Println("GF(1) should rise error")
-		return
-	}
-
-	pre := uint32(0)
-	for i := 0; i < len(x); i++ {
-		d := (x[i] ^ k[i]) ^ pre
-		y[i] = uint32(gfpoly.Exp(uint64(d), 6442450943))
-		pre = y[i]
-		log.Printf("X:%v, d:%v, y:%v", x[i], d, y[i])
-	}
-
-	pre = 0
-	for i := 0; i < len(y); i++ {
-		d := uint32(gfpoly.Exp(uint64(y[i]), 2))
-		x[i] = (d ^ pre) ^ k[i]
-		pre = y[i]
-		log.Printf("X:%v, d:%v, y:%v", x[i], d, y[i])
-	}
-}
-
 func test_encypt_decrypt() {
 	const PATH_TEST = "blocks.json"
 	w := wallet.NewWallet("blocks.json.wallet")
@@ -275,15 +65,113 @@ func test_encypt_decrypt() {
 	}
 	close(msg)
 }
-func test_bigint() {
-	a := big.NewInt(1)
-	b := big.NewInt(2)
-	c := big.NewInt(4)
-	log.Printf("%x, %x, %x", a, b, c)
-	a.Add(a, b)
-	log.Printf("%x, %x, %x", a, b, c)
-	c.Add(c, b)
-	log.Printf("%x, %x, %x", a, b, c)
+
+func test_encypt_2() {
+	gf := galois.NewGFP()
+	if gf == nil {
+		log.Println("GF(1) should rise error")
+		return
+	}
+
+	p := gf.Prime.ToBig()
+	p = p.Mul(p, big.NewInt(2))
+	p = p.Sub(p, big.NewInt(1))
+	p = p.Div(p, big.NewInt(3))
+	p = p.Mod(p, gf.Prime.ToBig())
+	I, _ := uint256.FromBig(p)
+	log.Printf("Inv : %x", I)
+
+	tenc := int64(0)
+	tdec := int64(0)
+
+	for k := 0; k < 10000; k++ {
+		size := 100
+		x := make([]uint64, size)
+		k := make([]uint64, len(x))
+		y := make([]*uint256.Int, len(x))
+		for i := 0; i < len(x); i++ {
+			x[i] = rand.Uint64()
+			k[i] = rand.Uint64()
+		}
+
+		pre := uint256.NewInt(0)
+		start := time.Now().UnixNano()
+		for i := 0; i < len(x); i++ {
+			xu := uint256.NewInt(x[i])
+			ku := uint256.NewInt(k[i])
+			d := gf.Add(xu, ku)
+			d = gf.Add(d, pre)
+			y[i] = gf.Exp(d, I)
+
+			pre = y[i]
+			// log.Printf("X:%v, d:%v, y:%v", x[i], d, y[i])
+		}
+		tenc += (time.Now().UnixNano() - start) / 1000000 // msec
+
+		pre = uint256.NewInt(0)
+		start = time.Now().UnixNano()
+		for i := 0; i < len(y); i++ {
+			ku := uint256.NewInt(k[i])
+			d := gf.Exp(y[i], uint256.NewInt(3))
+			d = gf.Sub(d, pre)
+			d = gf.Sub(d, ku)
+			pre = y[i]
+			if x[i] != d.Uint64() {
+				log.Printf("%v, X:%v, y:%v", i, x[i], d)
+				break
+			}
+
+		}
+		tdec += (time.Now().UnixNano() - start) / 1000000 // msec
+	}
+
+	log.Printf("enc : %v, dec : %v", tenc, tdec)
+}
+
+func test_encypt_decrypt_prime() {
+	const PATH_TEST = "blocks_360.json"
+	w := wallet.NewWallet("blocks.json.wallet")
+	key := w.PublicKey
+	addr := w.PublicKey
+
+	msg := make(chan bitcoin.BlockPkt)
+	go simulation.LoadBtcData(PATH_TEST, msg)
+
+	tenc := int64(0)
+	tdec := int64(0)
+	for {
+		d, ok := <-msg
+		if !ok {
+			log.Println("Channle closed")
+			break
+		}
+
+		if d.Block == config.END_TEST {
+			break
+		}
+
+		rb := bitcoin.NewRawBlock(d.Block)
+		x := rb.GetBlockBytes()
+		// log.Printf("Block : %v", x[:80])
+
+		// Start Encryption
+		start := time.Now().UnixNano()
+		_, y := poscipher.EncryptPoSWithPrimeField(key, poscipher.CalculateXorWithAddress(addr, x))
+		tenc += (time.Now().UnixNano() - start) / 1000000 // msec
+		log.Printf("Encryption Time : %v", tenc)
+		log.Printf("Enc x %v :%x", len(y), y[0:80])
+		// Start Decryption
+		start = time.Now().UnixNano()
+		x_t := poscipher.DecryptPoSWithPrimeField(key, y)
+		x_t = poscipher.CalculateXorWithAddress(addr, x_t)
+		tdec += (time.Now().UnixNano() - start) / 1000000 // msec
+		log.Printf("Decryption Time : %v", tdec)
+
+		log.Printf("Org x %v :%v", len(x), x[len(x)-80:])
+		log.Printf("New x %v :%v", len(x_t), x_t[len(x)-80:len(x)])
+		key = y
+	}
+	close(msg)
 }
 
 func test_fri_prove_low_degree() {
@@ -330,9 +218,10 @@ func main() {
 	// test_gf_exp3()
 	// test_gf_div2()
 	// test_encypt_2()
-	// test_encypt_decrypt()
+	// test_encypt_decrypt()?
 	// test_bigint()
 	// test_gf256()
 	// test_gf256_exp()
-	test_fri_prove_low_degree()
+	// test_fri_prove_low_degree()
+	test_encypt_decrypt_prime()
 }
--- common/poscipher/poscipher.go
+++ common/poscipher/poscipher.go
@@ -6,13 +6,18 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"log"
+	"math/big"
 
+	"github.com/holiman/uint256"
 	"github.com/junwookheo/bcsos/common/galois"
 )
 
 const ALIGN = 4
+const PALIGN = 8
 
 var GF = galois.GFN(32)
+var GFP = galois.NewGFP()
+var Ix3 = GetInverseX3()
 
 func GetHashString(buf []byte) string {
 	hash := sha256.Sum256(buf)
@@ -187,4 +192,158 @@ func CalculateXorWithAddress(addr, s []byte) []byte {
 	}
 
 	return d
+}
+
+const PSIZE = 31 // Prime field with 32byte so 31bytes is max to convert Prime field
+func GetInverseX3() *uint256.Int {
+	p := GFP.Prime.ToBig()
+	p = p.Mul(p, big.NewInt(2))
+	p = p.Sub(p, big.NewInt(1))
+	p = p.Div(p, big.NewInt(3))
+	p = p.Mod(p, GFP.Prime.ToBig())
+	I, _ := uint256.FromBig(p)
+	return I
+}
+
+func EncryptPoSWithPrimeField(key, s []byte) (string, []byte) {
+	lk := len(key) // assume key is aligned
+	if lk%PALIGN != 0 {
+		log.Panicf("Error Key length ALIGN: %v", lk)
+	}
+	ls := len(s)
+	lpad := ls % PSIZE
+	if lpad != 0 {
+		for i := 0; i < PSIZE-lpad; i++ { // Add Padding
+			s = append(s, 0x0)
+		}
+	}
+
+	ls = len(s)
+	if ls%PSIZE != 0 {
+		log.Panicf("Error Source length ALIGN : %v", ls)
+	}
+
+	lk /= PALIGN // 4bytes of key is used for encryption
+	ls /= PSIZE
+
+	key_reader := bytes.NewReader(key)
+	s_reader := bytes.NewReader(s)
+
+	k := make([]uint64, lk)
+	err := binary.Read(key_reader, binary.LittleEndian, &k)
+	if err != nil {
+		log.Panicf("converting key to uint32 error : %v", err)
+		return "", nil
+	}
+
+	x := make([][PSIZE]byte, ls)
+	err = binary.Read(s_reader, binary.LittleEndian, &x)
+	if err != nil {
+		log.Panicf("converting source to uint32 error : %v", err)
+		return "", nil
+	}
+
+	y := make([]*uint256.Int, ls)
+	pre := uint256.NewInt(1)
+	if lk < ls {
+		for i := 0; i < ls; i++ {
+			xu := uint256.NewInt(0)
+			xu.SetBytes31(x[i][:])
+			ku := uint256.NewInt(k[i%lk])
+			d := GFP.Add(xu, ku)
+			// d = GFP.Add(d, pre)
+			d = GFP.Div(d, pre)
+			y[i] = GFP.Exp(d, Ix3)
+			pre = y[i]
+		}
+	} else {
+		for i := 0; i < ls; i++ {
+			xu := uint256.NewInt(0)
+			xu.SetBytes31(x[i][:])
+			ku := uint256.NewInt(k[i])
+			d := GFP.Add(xu, ku)
+			d = GFP.Add(d, pre)
+			y[i] = GFP.Exp(d, Ix3)
+			pre = y[i]
+		}
+	}
+
+	buf := new(bytes.Buffer)
+	for i := 0; i < len(y); i++ {
+		err = binary.Write(buf, binary.LittleEndian, y[i].Bytes32())
+		if err != nil {
+			log.Panicf("convert uint32 to byte error : %v", err)
+			return "", nil
+		}
+	}
+
+	return GetHashString(buf.Bytes()), buf.Bytes()
+}
+
+func DecryptPoSWithPrimeField(key, s []byte) []byte {
+	lk := len(key) // assume key is aligned
+	if lk%PALIGN != 0 {
+		log.Panicf("Error Key length ALIGN: %v", lk)
+	}
+	ls := len(s)
+	if lk%(PSIZE+1) != 0 {
+		log.Panicf("Error Source length ALIGN : %v", ls)
+	}
+
+	lk /= PALIGN
+	ls /= (PSIZE + 1)
+
+	key_reader := bytes.NewReader(key)
+	s_reader := bytes.NewReader(s)
+
+	k := make([]uint64, lk)
+	err := binary.Read(key_reader, binary.LittleEndian, &k)
+	if err != nil {
+		log.Panicf("converting key to uint32 error : %v", err)
+		return nil
+	}
+
+	x := make([][32]byte, ls)
+	err = binary.Read(s_reader, binary.LittleEndian, &x)
+	if err != nil {
+		log.Panicf("converting source to uint32 error : %v", err)
+		return nil
+	}
+
+	y := make([]*uint256.Int, ls)
+	pre := uint256.NewInt(1)
+	if lk < ls {
+		for i := 0; i < ls; i++ {
+			xu := uint256.NewInt(0)
+			xu.SetBytes32(x[i][:])
+			ku := uint256.NewInt(k[i%lk])
+			d := GFP.Exp(xu, uint256.NewInt(3))
+			// d = GFP.Sub(d, pre)
+			d = GFP.Mul(d, pre)
+			y[i] = GFP.Sub(d, ku)
+			pre = xu
+		}
+	} else {
+		for i := 0; i < ls; i++ {
+			xu := uint256.NewInt(0)
+			xu.SetBytes32(x[i][:])
+			ku := uint256.NewInt(k[i%lk])
+			d := GFP.Exp(xu, uint256.NewInt(3))
+			d = GFP.Sub(d, pre)
+			y[i] = GFP.Sub(d, ku)
+			pre = xu
+		}
+	}
+
+	buf := new(bytes.Buffer)
+	for i := 0; i < len(y); i++ {
+		yu := y[i].Bytes32()
+		err = binary.Write(buf, binary.LittleEndian, yu[1:])
+		if err != nil {
+			log.Panicf("convert uint32 to byte error : %v", err)
+			return nil
+		}
+	}
+
+	return buf.Bytes()
 }
