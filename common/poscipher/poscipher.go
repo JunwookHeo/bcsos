2b067a63ae846606c6b1977c56061223762ff8ba
junwookheo <junwookheo@gmail.com>
2023-08-28 12:25:13 +1000

    Fix a build error

--- common/poscipher/poscipher.go
+++ common/poscipher/poscipher.go
@@ -84,7 +84,6 @@ func EncryptPoSWithVariableLength(key, s []byte) (string, []byte) {
 		return "", nil
 	}
 
-	log.Printf("ENC the number of zero : %v", cntzero)
 	return GetHashString(buf.Bytes()), buf.Bytes()
 }
 
